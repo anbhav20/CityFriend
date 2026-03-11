ff6fe1cf83e4501bb827171a3026332f376603a3
anbhav <your.email@example.com>
2026-03-11 11:42:48 +0530

    fix api and cors after deploy

--- server/src/app.js
+++ server/src/app.js
@@ -4,14 +4,17 @@ const authRoutes = require('../routes/authRoutes')
 const UserRoute= require('../routes/userRoutes')
 const cookieParser = require('cookie-parser')
 const postRoute = require('../routes/postRoutes')
+const path= require('path')
 
 const app = express();
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, "../public")))
+
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:5173"
+    origin: true //"http://localhost:5173"
   })
 );
 app.use(cookieParser())
@@ -19,6 +22,10 @@ app.use(cookieParser())
 app.use('/api/auth', authRoutes)
 app.use('/api', UserRoute)
 app.use('/api/posts', postRoute)
+
+app.get('*', (req, res)=>{
+  res.sendFile(path.join(__dirname, "../public/index.html"))
+})
 
 
 module.exports = app;
